18b1e3b08ed6e103de9f768b6378fe594ea5d26c
Tommy Rischmann <trischma@f2r7s1.clusters.42paris.fr>
2025-10-23 17:50:55 +0200

    Push automatique :)

--- objets/source/cgi/utils/CGIEnvironment.d
+++
@@ -1,8 +0,0 @@
-objets/source/cgi/utils/CGIEnvironment.o: \
-  source/cgi/utils/CGIEnvironment.cpp \
-  source/cgi/utils/../../../include/cgi/utils/CGIEnvironment.hpp \
-  source/cgi/utils/../../../include/http/Request.hpp
-
-source/cgi/utils/../../../include/cgi/utils/CGIEnvironment.hpp:
-
-source/cgi/utils/../../../include/http/Request.hpp:
