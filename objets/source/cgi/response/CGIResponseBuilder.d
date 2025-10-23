03b4f24587013f80707118815f7dc2aec6a3328e
Joseph Vittoz <jvittoz@f6r11s1.clusters.42paris.fr>
2025-10-23 12:54:44 +0200

    make clean

--- objets/source/cgi/response/CGIResponseBuilder.d
+++
@@ -1,8 +0,0 @@
-objets/source/cgi/response/CGIResponseBuilder.o: \
-  source/cgi/response/CGIResponseBuilder.cpp \
-  source/cgi/response/../../../include/cgi/response/CGIResponseBuilder.hpp \
-  source/cgi/response/../../../include/http/Response.hpp
-
-source/cgi/response/../../../include/cgi/response/CGIResponseBuilder.hpp:
-
-source/cgi/response/../../../include/http/Response.hpp:
