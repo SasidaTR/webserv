3b15d6d0a051188c0889de494f1a9c2617e7eea6
Tommy Rischmann <trischma@f6r10s2.clusters.42paris.fr>
2025-10-28 16:20:04 +0100

    Push automatique :)

---
+++ objets/source/http/handlers/DirectoryHandler.d
@@ -0,0 +1,5 @@
+objets/source/http/handlers/DirectoryHandler.o: \
+  source/http/handlers/DirectoryHandler.cpp \
+  source/http/handlers/../../../include/http/handlers/DirectoryHandler.hpp
+
+source/http/handlers/../../../include/http/handlers/DirectoryHandler.hpp:
