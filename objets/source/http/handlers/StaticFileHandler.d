989391c967756fad5934ac926d18c7de16c91a2d
Tommy Rischmann <trischma@f6r10s2.clusters.42paris.fr>
2025-10-28 16:20:11 +0100

    Push automatique :)

--- objets/source/http/handlers/StaticFileHandler.d
+++
@@ -1,8 +0,0 @@
-objets/source/http/handlers/StaticFileHandler.o: \
-  source/http/handlers/StaticFileHandler.cpp \
-  source/http/handlers/../../../include/http/handlers/StaticFileHandler.hpp \
-  source/http/handlers/../../../include/http/handlers/../Response.hpp
-
-source/http/handlers/../../../include/http/handlers/StaticFileHandler.hpp:
-
-source/http/handlers/../../../include/http/handlers/../Response.hpp:
