f476faebac5e7e705f48cfc063a0acc141198de2
Tommy Rischmann <trischma@f6r9s2.clusters.42paris.fr>
2025-10-29 14:46:45 +0100

    Push automatique :)

--- cgi/cgi-bin/delete.py
+++ cgi/cgi-bin/delete.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
-import os
-import sys
+import os, sys, glob
 
 method = os.environ.get('REQUEST_METHOD', 'GET')
-files_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'html', 'files')
-files_dir = os.path.abspath(files_dir)
+files_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../html/files"))
 
 print("Content-Type: text/plain\n")
 
 if method == 'DELETE':
-    txt_files = [f for f in os.listdir(files_dir) if f.endswith('.txt')]
-    if not txt_files:
-        print("No .txt files to delete")
-    else:
-        txt_files_paths = [os.path.join(files_dir, f) for f in txt_files]
-        latest_file = max(txt_files_paths, key=os.path.getmtime)
-        try:
-            os.remove(latest_file)
-            print(f"Deleted file: {os.path.basename(latest_file)}")
-        except Exception as e:
-            print("Error deleting file:", e)
+	txt_files = glob.glob(os.path.join(files_dir, "*.txt"))
+	if not txt_files:
+		print("No .txt files to delete")
+	else:
+		latest_file = max(txt_files, key=os.path.getmtime)
+		try:
+			os.remove(latest_file)
+			print(f"Deleted file: {os.path.basename(latest_file)}")
+		except Exception as e:
+			print("Error deleting file:", e)
 else:
-    print("Unsupported method:", method)
+	print("Unsupported method:", method)
